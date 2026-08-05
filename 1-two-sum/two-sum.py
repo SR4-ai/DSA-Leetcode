8e962ab8b294bdaf1beb7f61674d824381051afd
Sayantan Metya <sr4metya@gmail.com>
2026-08-05 19:48:15 +0530

    Time: 1 ms (61.64%) | Memory: 13.2 MB (52.89%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -14,3 +14,7 @@ class Solution(object):
                 return [seen[complement], i]
 
             seen[nums[i]] = i
+        '''for i in range(len(nums)):
+            for j in range(i+1,(len(nums))):
+                if (nums[i]+nums[j])==target:
+                    return [i,j]'''
