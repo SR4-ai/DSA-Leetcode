65c0ce538ea971fbfd605fd76954ce9888b83b6e
Sayantan Metya <sr4metya@gmail.com>
2026-08-06 20:37:51 +0530

    Time: 18 ms (21.04%) | Memory: 14.4 MB (7.91%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,5 +4,44 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        square_nums = list(map(lambda x:x**2,nums))
-        return sorted(square_nums)
+        # sq_nums = list(map(lambda x:x**2,nums))
+        neg=[]
+        pos=[]
+        res =[]
+        for i in range(len(nums)):
+            if(nums[i]>=0):
+                pos.append(nums[i])
+            else:
+                neg.append(nums[i])
+        if(len(pos)==0):
+            return [x * x for x in neg][::-1]
+
+        if(len(neg)==0):
+            return [x * x for x in pos]
+        
+        neg = [x * x for x in neg][::-1]
+        pos = [x * x for x in pos]
+        n,m = len(neg),len(pos)
+        i = 0
+        j= 0
+        
+        while(i<n and j<m):
+            if(neg[i] <= pos[j]):
+                res.append(neg[i])
+                
+                i+=1
+            else:
+                res.append(pos[j])
+            
+                j+=1
+
+        while(j<m):
+            res.append(pos[j])
+        
+            j+=1
+        while(i<n):
+            res.append(neg[i])
+          
+            i+=1
+
+        return res
