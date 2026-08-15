1cca17de6ee0e4c72a43fcc6108209a3f9e0f506
Sayantan Metya <sr4metya@gmail.com>
2026-08-15 20:27:38 +0530

    Time: 7 ms (94.03%) | Memory: 13.2 MB (54.93%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -13,6 +13,28 @@ class Solution(object):
                 curr = 1
         return max_product
         '''
+
+        '''
+        class Solution(object):
+    def maxProduct(self, nums):
+        max_product = nums[0]
+        curr_max = nums[0]
+        curr_min = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+
+            old_max = curr_max
+
+            curr_max = max(num, num * curr_max, num * curr_min)
+            curr_min = min(num, num * old_max, num * curr_min)
+
+            max_product = max(max_product, curr_max)
+
+        return max_product
+        '''
+
+    
         prefix = 1
         suffix = 1
         max_prod= float('-inf')
