7e0c169299befd4682606ff79c858533ff07966d
Sayantan Metya <sr4metya@gmail.com>
2026-09-23 13:55:47 +0530

    Time: 4 ms (42.66%) | Memory: 16.5 MB (44.83%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -1,22 +1,39 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# # Definition for a binary tree node.
+# # class TreeNode(object):
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution(object):
+#     def insertIntoBST(self, root, val):
+#         """
+#         :type root: Optional[TreeNode]
+#         :type val: int
+#         :rtype: Optional[TreeNode]
+#         """
+#         if root is None:
+#             return TreeNode(val)
+
+#         if val < root.val:
+#             root.left = self.insertIntoBST(root.left, val)
+#         else:
+#             root.right = self.insertIntoBST(root.right, val)
+
+#         return root
+
+### With helper function
 class Solution(object):
     def insertIntoBST(self, root, val):
-        """
-        :type root: Optional[TreeNode]
-        :type val: int
-        :rtype: Optional[TreeNode]
-        """
-        if root is None:
-            return TreeNode(val)
 
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        else:
-            root.right = self.insertIntoBST(root.right, val)
+        def insert(node):
+            if node is None:
+                return TreeNode(val)
 
-        return root
+            if val < node.val:
+                node.left = insert(node.left)
+            else:
+                node.right = insert(node.right)
+
+            return node
+
+        return insert(root)
